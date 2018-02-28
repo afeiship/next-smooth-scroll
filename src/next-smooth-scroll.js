93de2751d1b4a968c30fb4c04f2f48cbd33ce087
afeiship <1290657123@qq.com>
2018-02-28 20:41:35 +0800

    issue: optimze

--- src/next-smooth-scroll.js
+++ src/next-smooth-scroll.js
@@ -3,11 +3,15 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
 
-  nx.smoothScroll = function (inRate, inDistance) {
+  nx.smoothScroll = function (inDistance, inRate) {
+    var distance = inDistance || 0;
+    var rate = inRate || 0.8;
     var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (currentScroll > inDistance) {
-      global.requestAnimationFrame(nx.smoothScroll);
-      global.scrollTo(inDistance, inRate * currentScroll);
+    if (currentScroll > distance) {
+      global.requestAnimationFrame(function () {
+        nx.smoothScroll(distance, rate);
+      });
+      global.scrollTo(distance, rate * currentScroll);
     }
   };
 
