55fb71727ec29d2ea15bff4bd9edbfef2a457d63
afeiship <1290657123@qq.com>
2018-03-01 17:50:17 +0800

    issue: clean before next

--- dist/next-smooth-scroll.js
+++ dist/next-smooth-scroll.js
@@ -2,13 +2,15 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
+  var timer = null;
 
   nx.smoothScroll = function (inDistance, inRate) {
     var distance = inDistance || 0;
     var rate = inRate || 0.8;
     var current = document.documentElement.scrollTop || document.body.scrollTop;
     if (current > distance) {
-      global.requestAnimationFrame(function () {
+      timer && global.cancelAnimationFrame(timer);
+      timer = global.requestAnimationFrame(function () {
         nx.smoothScroll(distance, rate);
       });
       global.scrollTo(distance, rate * current);
