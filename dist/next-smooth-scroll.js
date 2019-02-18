e160401f817f59de025e5f7a2876fefc1897a943
afeiship <1290657123@qq.com>
2019-02-18 20:27:08 +0800

    fix: style

--- dist/next-smooth-scroll.js
+++ dist/next-smooth-scroll.js
@@ -1,5 +1,4 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var nxEaseout = nx.easeout || require('next-easeout');
@@ -8,7 +7,7 @@
   var NxSmoothScroll = nx.declare('nx.SmoothScroll', {
     properties: {
       bottomToScroll: {
-        get: function () {
+        get: function() {
           // https://stackoverflow.com/questions/34874658/get-the-scroll-distance-from-bottom-to-scroll
           var scrollPosition = window.pageYOffset;
           var windowSize = window.innerHeight;
@@ -18,19 +17,19 @@
       }
     },
     statics: {
-      config: function(inAccessor){
-        this.accessor= inAccessor || {
-          set: function(inValue){
+      config: function(inAccessor) {
+        this.accessor = inAccessor || {
+          set: function(inValue) {
             DOC.scrollTop = inValue;
           },
-          get: function(){
-            return DOC.scrollTop
+          get: function() {
+            return DOC.scrollTop;
           }
-        }
+        };
       },
-      easeout: function (inTo, inRate, inCallback) {
+      easeout: function(inTo, inRate, inCallback) {
         var callback = inCallback || nx.noop;
-        nxEaseout(this.accessor.get(), inTo, inRate, function (inValue, inRet) {
+        nxEaseout(this.accessor.get(), inTo, inRate, function(inValue, inRet) {
           this.accesgsor.set(inValue);
           inRet && callback();
         });
@@ -41,5 +40,4 @@
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxSmoothScroll;
   }
-
-}());
+})();
